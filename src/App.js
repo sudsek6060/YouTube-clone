d5926837c430c6419b4375aececdef358c86f4e7
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-05-17 19:50:09 +0530

    NavBar

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 import { Box } from '@mui/material';
 import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components'
 
-const App = () => {
+const App = () => (
  <BrowserRouter>
   <Box>
-    <Navbar />
+    <Navbar/>
     <Routes>
       <Route path="/" exact element={<Feed/>} />
-      <Route path="/videi/:id" element={<VideoDetail/>} />
+      <Route path="/video/:id" element={<VideoDetail/>} />
       <Route path="/channel/:id"  element={<ChannelDetail/>} />
       <Route path="/search/:searchTerm"  element={<SearchFeed/>} />
     </Routes>
   </Box>
  </BrowserRouter>
-}
+)
 
 export default App
