f6dcdb0337964e77af3680e4ab411edcc93c02ec
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-06-12 20:25:21 +0530

    videodetails and loader

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,11 +1,13 @@
 
 import { Stack, Box } from '@mui/material';
-import {VideoCard, ChannelCard} from './'
+import {VideoCard, ChannelCard, Loader} from './'
 
-const Videos = ({videos}) => {
+const Videos = ({videos, direction}) => {
+
+  if(!videos?.length) return <Loader />;
 
   return (
-    <Stack direction= "row" flexWrap="wrap" justifyContent= "start" gap={1}>
+    <Stack direction= {direction || "row"} flexWrap="wrap" justifyContent= "start" gap={1}>
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
